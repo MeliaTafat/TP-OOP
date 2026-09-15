87bab22ea88891815f76fdb2341063a899d75ee6
MeliaTafat <thafatm@gmail.com>
2026-09-15 15:00:42 +0200

    Question2 : Implementation de la fonction calibrer

--- tp0/tuples.py
+++ tp0/tuples.py
@@ -5,8 +5,28 @@ releves = [releve1, releve2, releve3]
 
 def afficher_releve(releve) :
     val1, val2, val3 = releve 
-    return f"Capteur {val1} : {val2} {val3}"   
+    return f"Capteur {val1} : {val2} {val3}"
 
+def recalibrer(releves, nom, valeur):
+    nouveaux_releves = []
+    for releve in releves:
+        if releve[0] == nom:
+            nouveaux_releves.append((releve[0], valeur, releve[2]))
+        else:
+            nouveaux_releves.append(releve)
+    return nouveaux_releves
+
+nouveaux_releves = recalibrer(releves, "laser_avant", 2.40)
+
+        
+        
+# Tests pour la question 1
 assert len(releves) == 3
 assert releves[0][0] == "laser_avant"
 assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
+
+#tests pour la question2 
+assert nouveaux_releves[0] == ("laser_avant", 2.40, "m")
+assert nouveaux_releves[1] == releve2
+assert nouveaux_releves[2] == releve3
+
