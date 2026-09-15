da93978bd54f6de3e9c3777b3d2f9140632f21d1
MeliaTafat <thafatm@gmail.com>
2026-09-15 14:36:11 +0200

    Question1 : Implementation de la fonction afficher

---
+++ tp0/tuples.py
@@ -0,0 +1,12 @@
+releve1 =  ("laser_avant", 2.35, "m")
+releve2 = ("laser_arriere", 1.10, "m")
+releve3 = ("gyroscope", 87.5, "deg")
+releves = [releve1, releve2, releve3]
+
+def afficher_releve(releve) :
+    val1, val2, val3 = releve 
+    return f"Capteur {val1} : {val2} {val3}"   
+
+assert len(releves) == 3
+assert releves[0][0] == "laser_avant"
+assert afficher_releve(releve1) == "Capteur laser_avant : 2.35 m"
