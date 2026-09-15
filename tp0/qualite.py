07ea890990068361aa507c9b497f1f2a2e51c72b
MeliaTafat <thafatm@gmail.com>
2026-09-15 15:56:12 +0200

    Question1 : Identification des problemes et correction de la fonction dans qualite.py

--- tp0/qualite.py
+++ tp0/qualite.py
@@ -1,15 +1,4 @@
-def f(d, t, x1, y1, x2, y2):
-    dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-    if t == ’R’:
-        c = dist * 1.0
-    elif t == ’H’:
-        c = dist * 1.5
-    elif t == ’S’:
-        c = dist * 2.0
-    else:
-        c = dist * 3.0
-    print("cout:", c)
-        return c
+
     
 # Problème 1 : nom de fonction "f" non explicite, ne décrit pas son rôle
 # Problème 2 : paramètre "d" jamais utilisé dans la fonction
