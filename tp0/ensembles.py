52fd7b1f9db16b53c0b4e51f6898a12d390c23c8
MeliaTafat <thafatm@gmail.com>
2026-09-15 15:16:10 +0200

    Question1 : Implementation des fonctions  dans ensembles.py

---
+++ tp0/ensembles.py
@@ -0,0 +1,19 @@
+robots_exploration = {"R2", "R5", "R7"}
+robots_transport = {"R5", "R9", "R7", "R3"}
+
+def robots_double_mission (ens1, ens2) :
+    return ens1 & ens2
+
+def robots_toutes_missions (ens1, ens2) :
+    return ens1 | ens2
+def robots_exploration_seulement(ens1, ens2) :
+    return ens1 - ens2
+
+double_mission = robots_double_mission(robots_exploration, robots_transport)
+toutes_missions = robots_toutes_missions(robots_exploration, robots_transport)
+exploration_seule = robots_exploration_seulement(robots_exploration, robots_transport)
+
+#Tests pour la question 1
+assert double_mission == {"R5", "R7"}
+assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
+assert exploration_seule == {"R2"}
