a8bce88c4d94570cecc0c206f22d237f22fe8061
MeliaTafat <thafatm@gmail.com>
2026-09-22 13:30:12 +0200

    Question 4.1 : Ajout des accesseurs et des mutateurs et rendre les attributs privés

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -1,24 +1,47 @@
 class Habitant :
     # un constructeur qui permet d'instancier un objet de classe Habitant
     def __init__(self, nom, age, adresse, animaux=None) :
-            self.nom = nom 
-            self.age = age
-            self.adresse = adresse
-            self.animaux = animaux if animaux is not None else {}
+            self.__nom = nom 
+            self.__age = age
+            self.__adresse = adresse
+            self.__animaux = animaux if animaux is not None else {}
+    
+    # Accesseurs
+    def get_nom(self) :
+        return self.__nom
+    
+    def get_age(self) :
+        return self.__age
+    
+    def get_adresse(self):
+        return self.__adresse
+    
+    def get_nombre(self, animal) :
+        return self.__animaux.get(animal, 0)
+    
+    def get_animaux(self):
+        return self.__animaux
+    
+    #Mutateurs
+    def set_nom(self, nom) :
+        self.__nom = nom
+    def set_age(self, nom) :
+        self.__age = nom
+    def set_adresse(self, adresse):
+        self.__adresse = adresse 
+    def set_animaux(self, animaux) :
+        self.__animaux = animaux
     
     #Methode qui affiche une chaine du type "Aldric habite a rue A"
     def affichage_adresse(self) :
-        print(f"{self.nom} habite a  {self.adresse}")
-    
-    # Accesseurs
-    def get_nombre(self, animal) :
-        return self.animaux.get(animal, 0)
+        print(f"{self.get_nom()} habite a {self.get_adresse()}")
+        
     # Methode qui renvoie le nombre d'animaux du type donné possedes pars l habitant 
     def compte_animal(self,animal) :
         return self.get_nombre(animal)
     
 h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
-assert h1.nom == "Aldric"
+assert h1.get_nom() == "Aldric"
 assert h1.compte_animal("vaches") == 3
 assert h1.compte_animal("moutons") == 0
 h1.affichage_adresse()
