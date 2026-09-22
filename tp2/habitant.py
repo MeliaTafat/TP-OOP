6fe461ef5a533d4cd250f418e7b01cedd7744519
MeliaTafat <thafatm@gmail.com>
2026-09-22 12:59:05 +0200

    Question 2 : Ajout des deux methodes affichage_adresse et compte_animal

--- tp2/habitant.py
+++ tp2/habitant.py
@@ -6,7 +6,20 @@ class Habitant :
             self.adresse = adresse
             self.animaux = animaux if animaux is not None else {}
     
-
+    #Methode qui affiche une chaine du type "Aldric habite a rue A"
+    def affichage_adresse(self) :
+        print(f"{self.nom} habite a  {self.adresse}")
     
-
+    # Accesseurs
+    def get_nombre(self, animal) :
+        return self.animaux.get(animal, 0)
+    # Methode qui renvoie le nombre d'animaux du type donné possedes pars l habitant 
+    def compte_animal(self,animal) :
+        return self.get_nombre(animal)
+    
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse()
         
